9a93ac7de8a1ae41f3d12a2cdc0025d4ddecdfb2
Jarek Andrzejczak <j.andrzejczak@gmail.com>
2018-05-30 00:05:58 +0200

    Add propTypes

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class Button extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  }
+
   render() {
     return <button className="btn">{this.props.title}</button>
   }
